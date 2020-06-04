c0aea315528ed9a1d8d7f20f2bca6c9ea385aaae
suho1991 <62007108+suho1991@users.noreply.github.com>
2020-06-04 17:56:37 +0900

    Add files via upload
    페이징위한 프레임워크

---
+++ DIVE/src/main/java/kr/jungang/dive/framework/paging/PageDTO.java
@@ -0,0 +1,32 @@
+package kr.jungang.dive.framework.paging;
+
+import lombok.Data;
+
+@Data
+public class PageDTO {
+	private static final int PAGING_LINK_AMOUNT = 15;
+	private int startPage;
+	private int endPage;
+	private boolean prev, next;
+	
+	private int total;
+	
+	private Criteria cri;
+	
+	public PageDTO(int total, Criteria cri) {
+		this.total = total;
+		this.cri = cri;
+		
+		this.endPage = (int) (Math.ceil(cri.getPageNum() / (float) PAGING_LINK_AMOUNT)) * 15;
+		this.startPage = this.endPage - (PAGING_LINK_AMOUNT - 1);
+		
+		int realEnd = (int) (Math.ceil((float) total / cri.getAmount())); 
+		
+		if (realEnd < this.endPage) {
+			this.endPage = realEnd;
+		}
+		
+		this.prev = this.startPage > 1;
+		this.next = this.endPage < realEnd; 
+	}
+}
