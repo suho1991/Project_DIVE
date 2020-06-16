345451a0d660534425e20cffeed059dea68081a5
jamie <kysspoon@naver.com>
2020-06-16 15:30:17 +0900

    css 파일 업데이트

--- Project_DIVE-master/DIVE/README.md
+++
@@ -1,2 +0,0 @@
-# Project_DIVE
-중고 거래 사이트
--- Project_DIVE-master/DIVE/src/main/webapp/resources/js/board.js
+++
@@ -1,8 +0,0 @@
-        const togglebtn = document.querySelector('.navbar_toggle_btn');
-        const menu = document.querySelector('.navbar_menu');
-        const icon = document.querySelector('.navbar_icons');
-
-        togglebtn.addEventListener('click', () => {
-            menu.classList.toggle('active');
-            icon.classList.toggle('active');
-        });
